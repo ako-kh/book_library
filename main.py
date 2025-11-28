c88a0c2e5e093fbb97ff9aa15e8c9de511c74d2a
Akaki Khorava <ako@Akakis-MacBook-Pro.local>
2025-11-28 14:36:14 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#todo book class
+
+"""
+todo sorted library class with
+
+function for importing books from json
+
+functions for serializing and deserializing library instance to binary/json
+
+functions for finding book by name, author, realis date
+
+function for printing data books as a grid
+"""
